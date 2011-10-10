6dbe48af36b4675fb8cd20dda8a46ace992fdd9d
Ian Hunt <ithunt0@gmail.com>
2011-10-09 21:12:38 -0400

    Adding constructor and getters/setters for Space

--- src/main/java/edu/rit/taskers/model/Space.java
+++ src/main/java/edu/rit/taskers/model/Space.java
@@ -11,5 +11,43 @@ public class Space {
 
     protected List<Contact> users;
 
+    public Space(String name, Date created, Contact creator, List<Contact> users) {
+        this.name = name;
+        this.created = created;
+        this.creator = creator;
+        this.users = users;
 
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getCreated() {
+        return created;
+    }
+
+    public void setCreated(Date created) {
+        this.created = created;
+    }
+
+    public Contact getCreator() {
+        return creator;
+    }
+
+    public void setCreator(Contact creator) {
+        this.creator = creator;
+    }
+
+    public List<Contact> getUsers() {
+        return users;
+    }
+
+    public void setUsers(List<Contact> users) {
+        this.users = users;
+    }
 }
