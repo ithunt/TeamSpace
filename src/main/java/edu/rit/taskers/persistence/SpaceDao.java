243f3717e2c5c6f3560cd4534083fcc9ef16d40e
ian <ithunt0@gmail.com>
2011-10-20 10:25:15 -0400

    Added SpaceDao

---
+++ src/main/java/edu/rit/taskers/persistence/SpaceDao.java
@@ -0,0 +1,51 @@
+package edu.rit.taskers.persistence;
+
+import edu.rit.taskers.model.Space;
+import edu.rit.taskers.model.Task;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.TaskScheduler;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+/**
+ * @author ian hunt
+ * @date 10.20.2011
+ */
+@Repository
+public class SpaceDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    public void setSessionFactory(SessionFactory sessionFactory) {
+        this.sessionFactory = sessionFactory;
+    }
+
+    @Transactional
+    public void save(Space space) {
+        this.sessionFactory.getCurrentSession().save(space);
+    }
+
+    @Transactional
+    public List<Space> findAll() {
+        return this.sessionFactory.getCurrentSession()
+                .createQuery("FROM Space").list();
+
+    }
+
+    @Transactional
+    public Space findById(int id) {
+        return (Space) sessionFactory.getCurrentSession().get(Space.class, id);
+    }
+
+    @Transactional
+    public Space findByName(String name) {
+        return (Space) sessionFactory.getCurrentSession()
+                .createQuery("FROM Space space where space.name=?")
+                .setParameter(0, name);
+    }
+
+}
